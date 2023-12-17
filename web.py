c724bab8044f845e8fc5fd1759ed62141fa12200
Yasir Arfat <yasirarfatlglglg@gmail.com>
2023-12-17 14:16:19 +0530

    Initial commit

---
+++ web.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import function
+def add_todo():
+    todo=st.session_state["new_todo"]
+    todos=function.add(todo)
+todos=function.get_todos()
+st.title("My todos web app")
+st.subheader("This is my todos app.")
+
+for index ,todo in enumerate(todos):
+    check_box= st.checkbox(todo,key=todo)
+    if check_box:
+        todos.pop(index)
+        function.write_todos(todos)
+        st.experimental_rerun()
+st.text_input("",placeholder="Add new todo..",
+              on_change=add_todo,
+              key="new_todo")
+
+st.button("Add")
